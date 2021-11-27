6ed5b8f32724b7322458e38a9a402aecb7dad824
unknown <vafaee.mh@gmail.com>
2021-11-27 11:17:38 +0330

    added styled footer

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,6 +1,15 @@
+import Footerright from "../footerRight/Footerright";
 import classes from "./Footer.module.scss";
+import ZarinPall from "../../img/zarin-pall.svg";
 const Footer = () => {
-  return <footer className={classes.footer}>footer</footer>;
+  return (
+    <footer className={classes.footer}>
+      <Footerright />
+      <div>
+        <img src={ZarinPall} alt="zarin" />
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
